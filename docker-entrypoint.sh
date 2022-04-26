18ebf2dd35e1260d08403a7ee873af8d9157e52b
Patrick Fasano <patrick@patrickfasano.com>
2022-04-26 13:02:13 -0400

    fixup! entrypoint: Use su-exec instead of gosu

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,7 +10,7 @@ _is_sourced() {
 
 # setup SSL certificates
 _setup_certificate() {
-	if [ -r "${SSL_CERTIFICATE}" && -r "${SSL_CERTIFICATE_KEY}" ]; then
+	if [ -r "${SSL_CERTIFICATE}" ] && [ -r "${SSL_CERTIFICATE_KEY}" ]; then
 		openssl pkcs12 -export -in "${SSL_CERTIFICATE}" -inkey "${SSL_CERTIFICATE_KEY}" -out "/etc/davmail/certs.p12" -passout pass:"davmail"
 	fi
 }
@@ -19,7 +19,7 @@ _main() {
 	_setup_certificate
 	if [ "$(id -u)" = '0' ]; then
 		# then restart script as davmail user
-		su-exec davmail:1000 "$BASH_SOURCE" "$@"
+		/sbin/su-exec davmail:1000 "$BASH_SOURCE" "$@"
 	fi
 
 	exec "$@"
