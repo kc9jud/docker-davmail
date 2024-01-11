5fad8b9eb5d87e79540fa4d53799361070ecad04
Patrick Fasano <patrick@patrickfasano.com>
2024-01-11 14:35:45 -0600

    entrypoint: Make compatible with busybox sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 # check to see if this file is being run or sourced from another script
-_is_sourced() {
-	# https://unix.stackexchange.com/a/215279
-	[ "${#FUNCNAME[@]}" -ge 2 ] \
-		&& [ "${FUNCNAME[0]}" = '_is_sourced' ] \
-		&& [ "${FUNCNAME[1]}" = 'source' ]
-}
+# DISABLED -- incompatible with busybox sh
+##_is_sourced() {
+##	# https://unix.stackexchange.com/a/215279
+##	[ "${#FUNCNAME[@]}" -ge 2 ] \
+##		&& [ "${FUNCNAME[0]}" = '_is_sourced' ] \
+##		&& [ "${FUNCNAME[1]}" = 'source' ]
+##}
 
 # setup SSL certificates
 _setup_certificate() {
@@ -28,6 +29,6 @@ _main() {
 	exec "$@"
 }
 
-if ! _is_sourced; then
+##if ! _is_sourced; then
 	_main "$@"
-fi
+##fi
